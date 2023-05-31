377a7b910c188774904bf1a7b11aafbf7904a24f
fauzandp <fauzan27dp@gmail.com>
2023-05-31 15:32:11 +0700

    Add existing file to Git

--- Project18/ViewController.swift
+++ Project18/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("I'm inside the viewDidLoad() method!")
+        print(1, 2, 3, 4, 5)
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: "")
+        print("Some message", "Some msg", "Some msg")
+
+        // fn + f6 to next line
+        // cmd + ctrl + y to next breakpoint
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+        
+        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
+        
+        assert(1 == 2, "Maths failure!")
+        assert(1 == 1, "Maths failure!")
+    }
+    
+    func myReallySlowMethod() -> Bool {
+        return false
     }
 
 
